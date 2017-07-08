7bd9b926d7e0c84450f1dddbce8cb8a9fcb102e6
Mazvydas Bartasius <15st3ps@gmail.com>
2017-07-09 01:00:45 +0200

    v1.1

--- Podfile
+++
@@ -1,7 +0,0 @@
-platform :ios, '10.0'
-use_frameworks!
-inhibit_all_warnings!
-
-target 'HapticFeedbackDemo'
-
-pod 'GBDeviceInfo', '4.3.0'
--- Pods/Target Support Files/GBDeviceInfo/GBDeviceInfo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GBDeviceInfo : NSObject
-@end
-@implementation PodsDummy_GBDeviceInfo
-@end
